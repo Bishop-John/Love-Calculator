a90d0c2e6582bdac00a3fc4bf6444791fea6e79e
Bishop-John <104781032+Bishop-John@users.noreply.github.com>
2023-03-12 09:22:26 +0000

    Add files via upload

---
+++ Love Calculator.py
@@ -0,0 +1,17 @@
+import random, time
+print ("Welcome to the Love Calculator")
+
+boyName = input("Enter the name of a boy")
+girlsName = input("Enter the name of a girl")
+result = random.randint(0, 100)
+
+print ("Calculating...")
+time.sleep(2)
+print ("Almost there...")
+time.sleep(2)
+print ("Your results are ready...")
+time.sleep(2)
+
+print ("You are a", Result, "% match for each other")
+input("Press enter key to exit...")
+exit()
